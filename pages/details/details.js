ce02194d59dfa829afd494156434337350087db7
yiwei wang <wywapple@gmail.com>
2021-12-08 08:38:24 +0800

    delete item component and merge into details page

--- pages/details/details.js
+++ pages/details/details.js
@@ -5,6 +5,48 @@ Page({
    * Page initial data
    */
   data: {
+    typeArray: [
+      {
+        name: 'spring',
+        num: 0,
+        selected: false,
+      },
+      {
+        name: 'summer',
+        num: 1,
+        selected: false,
+      },
+      {
+        name: 'autumn',
+        num: 2,
+        selected: false,
+      },
+      {
+        name: 'winter',
+        num: 3,
+        selected: false,
+      },
+      {
+        name: 'casual',
+        num: 4,
+        selected: false,
+      },
+      {
+        name: 'formal',
+        num: 5,
+        selected: false,
+      },
+      {
+        name: 'sporty',
+        num: 6,
+        selected: false,
+      },
+      {
+        name: 'work',
+        num: 7,
+        selected: false,
+      }
+    ]
 
   },
 
@@ -22,6 +64,20 @@ Page({
    * Lifecycle function--Called when page is initially rendered
    */
   onReady: function () {
+    console.log("ready")
+    let tag_list = this.data.item.tag_list;
+    let { typeArray } = this.data
+    tag_list.forEach(tag => {
+      // {
+      //   name: 'work',
+      //   num: 7,
+      //   selected: false,
+      // }
+      typeArray.filter(element => element.name === tag)[0].selected = true
+    })
+    console.log({typeArray})
+    this.setData({typeArray})
+    console.log('item component data', this.data)
 
   },
 
