e895a07956a00d66689e6c124d6bf7477465eb80
Yiyao Jia <fiona.jyy@hotmail.com>
2021-12-09 18:32:26 +0800

    change to cloud

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ App({
   globalData: {
     header: null,
     user: null,
-    // baseUrl: "https://de-closet-backend.wogengapp.cn/api/v1"
-    baseUrl: "http://localhost:3000/api/v1"
+    baseUrl: "https://de-closet-backend.wogengapp.cn/api/v1"
+    // baseUrl: "http://localhost:3000/api/v1"
   },
 })
